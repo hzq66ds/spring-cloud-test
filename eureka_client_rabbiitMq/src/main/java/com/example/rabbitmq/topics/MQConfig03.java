d1d96a71fec9061f5f24b5a8dc8207eeb9323716
hzq66ds <x_hzq@sina.com>
2019-07-02 01:09:32 +0800

    topics

---
+++ eureka_client_rabbiitMq/src/main/java/com/example/rabbitmq/topics/MQConfig03.java
@@ -0,0 +1,15 @@
+package com.example.rabbitmq.topics;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MQConfig03 {
+
+
+}
