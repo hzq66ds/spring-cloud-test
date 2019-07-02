ad8dd10a8f28c292b8a631c722a1384c4f518552
hzq66ds <x_hzq@sina.com>
2019-07-03 00:09:10 +0800

    rabbitMq Dockerfile

---
+++ eureka_client_rabbiitMq/buildDocker.sh
@@ -0,0 +1,29 @@
+#!/bin.bash
+
+export springboot_image=springboot_image_eureka_client_rabbitMq
+export imageid=`docker images|grep $springboot_image|grep latest|awk '{print $3}'`
+
+docker stop $springboot_image
+docker rm springboot_image
+
+echo "容器已停止，并移除start"
+docker ps -a|grep $springboot_image
+echo "容器已停止，并移除end"
+
+
+docker tag $imageid $springboot_image:`date +%Y%m%d%H%M%S`
+docker rmi $springboot_image:latest
+
+echo "镜像已重命名，start"
+docker images|grep $springboot_image
+echo "镜像已重命名，并移除end"
+
+echo "重新创建镜像，start"
+docker build -t $springboot_image .
+docker images
+echo "重新创建镜像，并移除end"
+
+docker run -d -p 8183:8183 --name $springboot_image $springboot_image
+
+
+
