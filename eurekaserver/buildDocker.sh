d25949ff5f783b1f88598cd9b2f4e03737acaf1c
韩志强 <x_hzq@sina.com>
2019-07-02 19:41:31 +0800

    脚本矫正

--- eurekaserver/buildDocker.sh
+++ eurekaserver/buildDocker.sh
@@ -7,6 +7,7 @@ export id8762=`docker images|grep $springboot_image_01|grep latest|awk '{print $
 export id8763=`docker images|grep $springboot_image_02|grep latest|awk '{print $3}'`
 
 docker stop $springboot_image_01 $springboot_image_02
+docker rm $springboot_image_01 $springboot_image_02
 
 docker tag $id8762 $springboot_image_01:`date +%Y%m%d%H%M%S`
 docker tag $id8763 $springboot_image_02:`date +%Y%m%d%H%M%S`
@@ -16,8 +17,8 @@ docker rmi $springboot_image_01:latest $springboot_image_02:latest
 docker build -t $springboot_image_01 -f $Dockerfile_01 .
 docker build -t $springboot_image_02 -f $Dockerfile_02 .
 
-docker run --rm -p 8762:8762 --name $springboot_image_01 $springboot_image_01
-docker run --rm -p 8763:8763 --name $springboot_image_02 $springboot_image_02
+docker run -d -p 8762:8762 --name $springboot_image_01 $springboot_image_01
+docker run -d -p 8763:8763 --name $springboot_image_02 $springboot_image_02
 
 
 
