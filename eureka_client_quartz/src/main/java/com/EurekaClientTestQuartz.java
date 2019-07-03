f87187b7f937ba97eccf30b2117c6268be16138b
韩志强 <x_hzq@sina.com>
2019-07-03 14:45:49 +0800

    quartz数据源配置修改

--- eureka_client_quartz/src/main/java/com/EurekaClientTestQuartz.java
+++ eureka_client_quartz/src/main/java/com/EurekaClientTestQuartz.java
@@ -17,52 +17,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-
+/**
+ * Created by hanzhiqiang@lenztechretail.com
+ * on 2019/7/3.
+ * @author HZQ-PC
+ */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.dbEntity.member.basicmappers","my"})
 public class EurekaClientTestQuartz {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientTestQuartz.class, args);
 	}
-
-
-	int[] sort(int[] arr1,int[] arr2){
-		int[] result = new int[arr1.length+arr2.length];
-
-		for (int i=0;i<arr1.length;i++) {
-			result[i] = arr1[i];
-		}
-		for (int i=0;i<arr2.length;i++) {
-			result[arr1.length+i] = arr2[i];
-		}
-
-		for (int i = 0; i < result.length; i++) {
-			for (int j = i; j < result.length; j++){
-				if (result[i]>result[j]){
-					int tempno = result[i];
-					result[i] = result[j];
-					result[j] = tempno;
-				}
-			}
-
-		}
-		return result;
-	}
-
-	String reverseString(String string,String oldStr,String replaceStr){
-		char[] list = string.toCharArray();
-
-		for (int num=0; num <= (list.length+1)/2;num++) {
-			if (num>list.length-num){
-				break;
-			}
-			char temp = list[num];
-			list[num] = list[list.length-num-1];
-			list[list.length-num-1] = temp;
-		}
-		return String.valueOf(list).replace(oldStr,replaceStr);
-	}
-
-
 }
